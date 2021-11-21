8e5921818e5654a80915e4f5dbc5a5e2665220ed
Ithilas <Bannow7@gmail.com>
2021-11-21 16:42:33 +0100

    Added vue framework and bootstrap

---
+++ dandd-app/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+createApp(App).use(router).use(BootstrapVue).use(BootstrapVueIcons).mount('#app')
