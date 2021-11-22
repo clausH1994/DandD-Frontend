119490c5e7ecf58f54773d018490efe909f1488b
Ithilas <Bannow7@gmail.com>
2021-11-22 13:04:46 +0100

    added footer to app

---
+++ dandd-app/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footer">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'footer',
+}
+</script>
+
+<style>
+
+.footer {
+    background-color: #A05353;
+    height: 80px;
+}
+
+</style>
