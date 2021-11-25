f7e8fd6e4107be3c2718e024a64fe863597fcd5f
blackjuiceXD <claushundeboel@hotmail.com>
2021-11-25 16:00:19 +0100

    view + static user load from database

--- dandd-app/src/views/profile/Profil.vue
+++ dandd-app/src/views/profile/Profil.vue
@@ -2,10 +2,50 @@
   <div class="profile">
     <div class="top">
       <div class="overskrift">
-    <h1> Profil </h1>
+        <h1>Profil</h1>
       </div>
       <div class="filler"></div>
-      <div class="knap">
+      <div class="knap"></div>
+    </div>
+    <div class="test">
+      <div class="placement">
+        <div class="card">
+          <div class="columm">
+            <p>Brugernavn: {{ username }}</p>
+            <p>Alder: {{ age }}</p>
+            <p>Role: {{ role }}</p>
+            <div style="display: flex;">
+              <p>Class: </p>
+               <div v-for="item in classes" v-bind:key="item.id">
+                 <p> &nbsp; {{item}},</p>
+               </div>
+            </div>
+           
+            
+          </div>
+
+          <div class="columm">
+            <p>Navn: {{ name }}</p>
+            <p>By: {{ city }}</p>
+            
+             <div style="display: flex;">
+              <p>setting: </p>
+               <div v-for="item in setting" v-bind:key="item.id">
+                 <p> &nbsp; {{item}},</p>
+               </div>
+            </div>
+            <p></p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <button>
+          <router-link to="/minekampagner">Mine Kampagner</router-link>
+        </button>
+        <button>
+          <router-link to="/rediger">Redigere Profil</router-link>
+        </button>
       </div>
     </div>
   </div>
@@ -13,8 +53,76 @@
 
 <script>
 export default {
+  // run when page is created and check if the user are logged in.
+  // calls getLoginUser()
+  created() {
+    //this.token = sessionStorage.getItem("user_token");
+    //this.userID = sessionStorage.getItem("user_id");
+    /*  if (this.token == null && this.userID == null) {
+      this.$router.push("Login");
+    } else {
+      this.getLoginUser();
+    } */
+    this.getLoginUser();
+    
+  },
 
-}
+  data: () => ({
+    userID: "619f9792f0d2ed7a8483fd91",
+
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5kayIsImlkIjoiNjE5Y2I5Yzc3ZjgwY2MwM2FhYmViYzFmIiwiaWF0IjoxNjM3ODQ3MTgxLCJleHAiOjE2Mzc5MzM1ODF9.7X6VgQwqDsojBVjHnx-uVGrC0pHfqI6G0hZUgvDKT9w",
+    username: null,
+    name: null,
+    age: null,
+    city: null,
+    zipcode: null,
+    role: null,
+    classes: null,
+    setting: null,
+
+  }),
+
+  methods: {
+    //gets logged in user from database.
+    getLoginUser() {
+      this.userID;
+      fetch("https://dandd-api.herokuapp.com/api/users/" + this.userID, {
+        method: "GET",
+        headers: { "auth-token": this.token },
+      }).then((response) =>
+        response
+          .json()
+          .then((data) => ({
+            data: data,
+            status: response.status,
+          }))
+          .then((response) => {
+            if (response.data) {
+              this.user = response.data;
+              this.username = response.username;
+              this.name = this.user.name;
+              this.age = this.user.age;
+              this.city = this.user.city;
+              this.zipcode = this.user.zipcode;
+              this.role = this.user.role;
+              this.classes = this.user.class;
+              this.setting = this.user.setting;
+            } else {
+              alert(
+                "Server returned " +
+                  response.status +
+                  " : " +
+                  response.statusText
+              );
+            }
+          })
+      );
+    },
+
+   
+  },
+};
 </script>
 
 <style>
@@ -24,5 +132,50 @@ export default {
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
+}
+
+.placement {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+.card {
+  background-color: #dedbc4;
+  display: flex;
+  width: 50%;
+  color: black;
+  border-radius: 35px/90px;
+  padding-top: 70px;
+  padding-bottom: 70px;
+}
+
+.columm {
+  width: 50%;
+  text-align: left;
+  margin-left: 75px;
+}
+
+p {
+  font-size: 30px;
+}
+
+button {
+  font-family: "Charm", cursive;
+  font-size: 30px;
+  background-color: #b93b3b;
+  color: white;
+  margin: 50px 17.5px;
+  border-radius: 12px;
+  width: 300px;
+}
+
+button a {
+  color: white;
+  text-decoration: none;
+}
+
+.test {
+  height: 67vh;
 }
 </style>
