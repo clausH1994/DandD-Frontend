5c9d5a395823ddef532e628241411bb11c4abdb3
Ithilas <Bannow7@gmail.com>
2021-12-15 14:01:29 +0100

    working on filter methods for displaying selected campaigns

--- dandd-app/src/views/Home.vue
+++ dandd-app/src/views/Home.vue
@@ -11,29 +11,33 @@
     </div>
 
     <div class="contenthome">
-      <div class="filter">
+      <div class="filter" id="filterBox">
         <h3>Advanced Filter</h3>
         <input type="text" placeholder="Søgefilter"><br>
         <div class="checkboxes">
           <h4 class="check" @click="openEdition = !openEdition">Edition</h4>
           <div class="edition" v-if="openEdition">
-            <div>
-            <input type="checkbox" name="1st" id="1st"><label>1st</label>
+            <div v-for="option in edition" v-bind:key="option">
+                  <input :id="option" type="checkbox" :value="option" v-model="checked.edition">
+                  <label :for="option" >{{ option }}</label>
             </div>
             <div>
-            <input type="checkbox" name="2nd" id="2nd"><label>2nd</label>
+            <input type="checkbox" name="1st" value="1st" id="1st" v-model="checked.edition"><label>1st</label>
             </div>
             <div>
-            <input type="checkbox" name="3rd" id="3rd"><label>3rd</label>
+            <input type="checkbox" name="2nd" value="2nd" id="2nd" v-model="checked.edition"><label>2nd</label>
             </div>
             <div>
-            <input type="checkbox" name="3.5" id="3.5"><label>3.5</label>
+            <input type="checkbox" name="3rd" value="3rd" id="3rd" v-model="checked.edition"><label>3rd</label>
             </div>
             <div>
-            <input type="checkbox" name="4th" id="4th"><label>4th</label>
+            <input type="checkbox" name="3.5" value="3.5" id="3.5" v-model="checked.edition"><label>3.5</label>
             </div>
             <div>
-            <input type="checkbox" name="5th" id="5th"><label>5th</label>
+            <input type="checkbox" name="4th" value="4th" id="4th" v-model="checked.edition"><label>4th</label>
+            </div>
+            <div>
+            <input type="checkbox" name="5th" value="5th" id="5th" v-model="checked.edition"><label>5th</label>
             </div>
           </div>
         </div>
@@ -41,66 +45,71 @@
           <h4 class="check" @click="openSetting = !openSetting">Fortrukne Setting</h4>
           <div class="setting" v-if="openSetting">
             <div>
-            <input type="checkbox" name="mordern" id="modern"><label>Modern</label>
+            <input type="checkbox" name="mordern" value="modern" id="modern" v-model="filterItems"><label>Modern</label>
             </div>
             <div>
-            <input type="checkbox" name="sci-fi" id="sci-fi"><label>Sci-fi</label>
+            <input type="checkbox" name="sci-fi" value="scifi" id="scifi" v-model="filterItems"><label>Sci-fi</label>
             </div>
             <div>
-            <input type="checkbox" name="fantasy" id="fantasy"><label>Fantasy</label>
+            <input type="checkbox" name="fantasy" value="fantasy" id="fantasy" v-model="filterItems"><label>Fantasy</label>
             </div>
             <div>
-            <input type="checkbox" name="homebrew" id="homebrew"><label>Homebrew</label>
+            <input type="checkbox" name="homebrew" value="homebrew" id="homebrew" v-model="filterItems"><label>Homebrew</label>
             </div>
           </div>
         </div>
         <div class="checkboxes">
           <h4 class="check" @click="openClasses = !openClasses">Ønskede Classes</h4>
           <div class="classes" v-if="openClasses">
-            <div>
-            <input type="checkbox" name="barbarian" id="barbarian"><label>Barbarian</label>
+            <div v-for="option in wishedClasses" v-bind:key="option">
+                  <input :id="option" type="checkbox" :value="option" v-model="checked.wishedClasses">
+                  <label :for="option" >{{ option }}</label>
             </div>
             <div>
-            <input type="checkbox" name="bard" id="bard"><label>Bard</label>
+            <input type="checkbox" name="barbarian" value="barbarian" id="barbarian" v-model="filterItems"><label>Barbarian</label>
             </div>
             <div>
-            <input type="checkbox" name="cleric" id="cleric"><label>Cleric</label>
+            <input type="checkbox" name="bard" value="bard" id="bard" v-model="filterItems"><label>Bard</label>
             </div>
             <div>
-            <input type="checkbox" name="druid" id="druid"><label>Druid</label>
+            <input type="checkbox" name="cleric" value="cleric" id="cleric" v-model="filterItems"><label>Cleric</label>
             </div>
             <div>
-            <input type="checkbox" name="fighter" id="fighter"><label>Fighter</label>
+            <input type="checkbox" name="druid" value="druid" id="druid" v-model="filterItems"><label>Druid</label>
             </div>
             <div>
-            <input type="checkbox" name="monk" id="monk"><label>Monk</label>
+            <input type="checkbox" name="fighter" value="fighter" id="fighter" v-model="filterItems"><label>Fighter</label>
             </div>
             <div>
-            <input type="checkbox" name="paladin" id="paladin"><label>Paladin</label>
+            <input type="checkbox" name="monk" value="monk" id="monk" v-model="filterItems"><label>Monk</label>
             </div>
             <div>
-            <input type="checkbox" name="ranger" id="ranger"><label>Ranger</label>
+            <input type="checkbox" name="paladin" value="paladin" id="paladin" v-model="filterItems"><label>Paladin</label>
             </div>
             <div>
-            <input type="checkbox" name="rogue" id="rogue"><label>Rogue</label>
+            <input type="checkbox" name="ranger" value="ranger" id="ranger" v-model="filterItems"><label>Ranger</label>
             </div>
             <div>
-            <input type="checkbox" name="sorcerer" id="sorcerer"><label>Sorcerer</label>
+            <input type="checkbox" name="rogue" value="rogue" id="rogue" v-model="filterItems"><label>Rogue</label>
             </div>
             <div>
-            <input type="checkbox" name="warlock" id="warlock"><label>Warlock</label>
+            <input type="checkbox" name="sorcerer" value="sorcerer" id="sorcerer" v-model="filterItems"><label>Sorcerer</label>
             </div>
             <div>
-            <input type="checkbox" name="wizard" id="wizard"><label>Wizard</label>
+            <input type="checkbox" name="warlock" value="warlock" id="warlock" v-model="filterItems"><label>Warlock</label>
             </div>
             <div>
-            <input type="checkbox" name="artificer" id="artificer"><label>Artificer</label>
+            <input type="checkbox" name="wizard" value="wizard" id="wizard" v-model="filterItems"><label>Wizard</label>
+            </div>
+            <div>
+            <input type="checkbox" name="artificer" value="artificer" id="artificer" v-model="filterItems"><label>Artificer</label>
             </div>
           </div>
         </div>
           
           <h4>Lokation</h4>
           <input type="text" placeholder="Lokation">
+
       </div>
 
       <div class="maincontent">
@@ -167,7 +176,7 @@
             </ul>
         </div>
 
-        <div class="campaigncard" v-for="campaign in campaigns" v-bind:key="campaign.id">
+        <div class="campaigncard" v-for="campaign in campaigns" v-bind:key="campaign.id" v-show="visible(campaign.edition, campaign.setting, campaign.wishedClasses)">
             <h3>{{ campaign.titel }}</h3>
             <ul class="campcard">
               <li>
@@ -239,6 +248,7 @@
 </template>
 
 <script>
+import _ from 'lodash';
 
 export default {
   name: 'Home',
@@ -249,8 +259,28 @@ export default {
       openSetting: false,
       openClasses: false,
       campaigns: [],
+      checked: {
+        edition: [],
+        setting: [],
+        wishedClasses: [],
+      },
     };
+
     },
+
+computed: {
+
+    edition: function() {
+      return this.availableTwo('edition').sort((a, b) => a - b);
+    },
+    setting: function() {
+      return this.available('setting').sort((a, b) => a - b);
+    },
+    wishedClasses: function() {
+      return this.available('wishedClasses').sort((a, b) => a < b ? -1 : 1);
+    }
+
+},
 
 methods: {
   getCampaigns() {
@@ -279,11 +309,49 @@ methods: {
           })
       );
     },
+
+    available: function(category) {
+      const categorySet = new Set([]);
+      for (var i = 0; i < this.campaigns.length; i++) {
+        console.log(category)
+        this.campaigns[i][category].forEach(el => categorySet.add(el));
+      }
+      return [...categorySet];
+    },
+    availableTwo: function(categoryTwo) {
+      const categorySet = new Set([]);
+      for (var i = 0; i < this.campaigns.length; i++) {
+        console.log(categoryTwo)
+        this.campaigns[i][categoryTwo].forEach(el => categorySet.add(el));
+        console.log(categoryTwo)
+      }
+      return [...categorySet];
+    },
+    visible: function(edition, setting, wishedClasses) {
+      const editions = this.checked.edition.length ? _.intersection(edition, this.checked.edition).length : true;
+      const settings = this.checked.setting.length ? _.intersection(setting, this.checked.setting).length : true;
+      const nclasses = this.checked.wishedClasses.length ? _.intersection(wishedClasses, this.checked.wishedClasses).length : true;
+      if (editions && settings && nclasses) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+
 },
+
+filters: {
+    capitalize: function (value) {
+      if (!value) return '';
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    }
+  },
 
 created() {
   this.getCampaigns();
-}
+
+},
 
 /* lav filter basered på getCampaigns og array fra checkboxes på siden for at kun vise ting der er valgt i checkbox som filter */
 };
@@ -353,6 +421,7 @@ h4 {
     color: black;
     margin-bottom: 20px;
     margin: 25px;
+    max-height: 420px;
 }
 
 .campaigncard h3 {
